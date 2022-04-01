86ab35aabeb15bad659e63e9963b50073b0abd38
Сергей Косолапов <serega.kosolapov2003@gmail.com>
2022-04-01 17:07:17 +0500

    Добавьте файлы проекта.

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,24 @@
+﻿#include <GL/glew.h>
+#include <iostream>
+#include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT); //очистка цвета
+    glutSwapBuffers(); //фоновый буфер меняется с буфером кадра
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); //инициализация GLUT
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA); //настройка опций (двойная буферизация и буфер цвета)
+    glutInitWindowSize(1024, 768);  //создание и изменения окна
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 1");
+    glutDisplayFunc(RenderSceneCB); //взаимодействие с оконной системой
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //вызов цвета
+
+    glutMainLoop(); //ждет события и передает их через функ обратного вызова
+}
